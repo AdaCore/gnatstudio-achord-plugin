b0e6fdfbedd71e521f4fa824f6b9a3f3adca62fa
Nicolas Setton <setton@adacore.com>
2021-09-08 14:14:43 +0100

    Add support for a homogeneous representation of file names
    All Achord code element file names should be stored
    relative to the project directory - this is needed for relocatability
    of Achord work trees.

---
+++ achord-integration/achord/project_support.py
@@ -0,0 +1,13 @@
+# Various utilities for project support
+
+import os
+
+
+def make_path_project_relative(filename):
+    try:
+        import GPS
+
+        project_dir = os.path.dirname(GPS.Project.root().filename())
+        return os.path.relpath(filename, start_dir=project_dir)
+    except Exception:
+        return filename
